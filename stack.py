571a749fd37992b9854e4b8fa84ca833f23f2476
Aleksey Semenov <7.on.off@bk.ru>
2020-04-04 16:11:14 +0300

    Исправление.
    Исправляет ошибку.

--- stack.py
+++ stack.py
@@ -3,17 +3,17 @@ class Stack:
         self.stack = []
 
     def is_empty(self):
-        return bool(self.stack)
+        return not bool(self.stack)
 
     def push(self, element):
         self.stack.insert(0, element)
 
     def pop(self):
-        if self.is_empty():
+        if not self.is_empty():
             return self.stack.pop(0)
 
     def peek(self):
-        if self.is_empty():
+        if not self.is_empty():
             return self.stack[0]
 
     def size(self):
